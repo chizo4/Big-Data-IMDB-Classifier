271bb9bdd6db716a0dceae87c2e480a0d99ee1eb
chizo <cierkoszfilip@gmail.com>
2025-03-16 17:07:58 +0100

    Cleanup for preprocess() into Pipeline.

--- movie-pipeline/data_utils.py
+++ movie-pipeline/data_utils.py
@@ -4,9 +4,9 @@ FILE:
     movie-pipeline/data_utils.py
 
 INFO:
-    Utily class for data implementing core tools associated with
+    Utily class for data implementing atomic tools associated with
     loading data from CSV and JSON files into Spark DataFrames,
-    saving results to disk, etc.
+    pre-processing sub-steps, or feature engineering.
 
 AUTHOR:
     @chizo4 (Filip J. Cierkosz)
@@ -21,7 +21,8 @@ import glob
 from logger import get_logger
 import numpy as np
 import pandas as pd
-from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml import Pipeline as SparkPipeline
+from pyspark.ml.feature import VectorAssembler, StandardScaler, StringIndexer, OneHotEncoder
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, when, log1p
 import re
@@ -32,7 +33,8 @@ class DataUtils:
     '''
     -------------------------
     DataLoader - A utility class for core data-related tasks; e.g., loading data
-                 from CSV and JSON files into Spark DataFrames, or saving results.
+                 from CSV and JSON files into Spark DataFrames, atomic pre-processing
+                 procedures, feature engineering, etc.
     -------------------------
     '''
 
@@ -157,56 +159,6 @@ class DataUtils:
         return data_dict
 
     @staticmethod
-    def preprocess_numeric_cols(df: 'DataFrame', cols: list, num_type: str = 'integer') -> 'DataFrame':
-        '''
-        Handle pre-processing operations for numeric columns.
-
-            Parameters:
-            -----------
-            df : DataFrame
-                The input DataFrame to preprocess.
-            cols : list
-                The list of numeric column names to preprocess.
-            num_type : str
-                The target numeric type for conversion.
-
-            Returns:
-            -----------
-            df : DataFrame
-                The preprocessed DataFrame.
-        '''
-        for col_name in cols:
-            # Convert missing values to None before type conversion. Required for Spark type casting operations.
-            df = df.withColumn(col_name, when(col('runtimeMinutes') == '\\N', None).otherwise(col(col_name)))
-            # Convert the current column to the proper numeric type: INT.
-            df = df.withColumn(col_name, col(col_name).cast(num_type))
-        return df
-
-    @staticmethod
-    def inject_median_values(df: 'DataFrame', col_name: str) -> 'DataFrame':
-        '''
-        Inject median values for missing entries in a numeric column.
-
-            Parameters:
-            -----------
-            df : DataFrame
-                The input DataFrame to preprocess.
-            col_name : str
-                The name of the column to be processed.
-
-            Returns:
-            -----------
-            df : DataFrame
-                The DataFrame with imputed median values.
-        '''
-        # Compute median for the target column.
-        col_median_int = int(df.approxQuantile(col_name, [0.5], 0.0)[0])
-        DataUtils.logger.info(f'Median: {col_name} = {col_median_int}')
-        # Fill missing values with the computed median.
-        df = df.withColumn(col_name, when(col(col_name).isNull(), col_median_int).otherwise(col(col_name)))
-        return df
-
-    @staticmethod
     def preprocess_text(text: str) -> str:
         '''
         Cleans a given text string by:
@@ -234,50 +186,121 @@ class DataUtils:
         return text.strip().title()
 
     @staticmethod
-    def preprocess(df: 'DataFrame') -> 'DataFrame':
+    def preprocess_numeric_cols(df: 'DataFrame', cols: list, num_type: str = 'integer') -> 'DataFrame':
         '''
-        Preprocess the data by handling missing values, fixing data
-        types, normalizing text fields, handling outliers, etc.
-
-        FULL PROCEDURE:
-        (1) Pre-process numeric columns by converting them to integer type.
-        (2) Inject median values for missing records: runtimeMinutes and numVotes.
-        (3) Log-transform numVotes to reduce high skewness.
-        (4) Ensure startYear ≤ endYear and handle missing values.
-        (5) Normalize text fields ("primaryTitle", "originalTitle").
+        Handle pre-processing operations for numeric columns.
 
             Parameters:
             -----------
             df : DataFrame
-                The input dataframe to preprocess.
+                The input DataFrame to preprocess.
+            cols : list
+                The list of numeric column names to preprocess.
+            num_type : str
+                The target numeric type for conversion.
 
             Returns:
             -----------
             df : DataFrame
                 The preprocessed DataFrame.
         '''
-        # (1) Pre-process numeric columns.
-        numeric_cols = ['runtimeMinutes', 'numVotes', 'startYear', 'endYear']
-        df = DataUtils.preprocess_numeric_cols(df, numeric_cols)
-        # (2) Inject median values for missing records: "runtimeMinutes" and "numVotes".
-        df = DataUtils.inject_median_values(df, 'runtimeMinutes')
-        df = DataUtils.inject_median_values(df, 'numVotes')
-        # (3) Log-transform "numVotes" to reduce skewness.
-        df = df.withColumn('numVotes', log1p(col('numVotes')))
-        # (4) Ensure startYear ≤ endYear and handle missing values.
-        df = df.withColumn('startYear', when(col('startYear').isNull(), col('endYear')).otherwise(col('startYear')))
-        df = df.withColumn('endYear', when(col('endYear').isNull(), col('startYear')).otherwise(col('endYear')))
-        df = df.withColumn('endYear', when(col('endYear') < col('startYear'), col('startYear')).otherwise(col('endYear')))
-        # (5) Normalize text title fields and handle missing records.
-        df = df.toPandas()
-        df['primaryTitle'] = df['primaryTitle'].apply(DataUtils.preprocess_text)
-        df['originalTitle'] = df['originalTitle'].apply(DataUtils.preprocess_text)
-        df['primaryTitle'] = df.apply(
-            lambda row: row['originalTitle'] if pd.isna(row['primaryTitle']) else row['primaryTitle'], axis=1
-        )
-        df['originalTitle'] = df.apply(
-            lambda row: row['primaryTitle'] if pd.isna(row['originalTitle']) else row['originalTitle'], axis=1
-        )
-        # Back to Spark DataFrame.
-        df = SparkSession.builder.getOrCreate().createDataFrame(df)
+        for col_name in cols:
+            # Convert missing values to None before type conversion. Required for Spark type casting operations.
+            df = df.withColumn(col_name, when(col('runtimeMinutes') == '\\N', None).otherwise(col(col_name)))
+            # Convert the current column to the proper numeric type: INT.
+            df = df.withColumn(col_name, col(col_name).cast(num_type))
+        return df
+
+    @staticmethod
+    def calc_median_col(df: 'DataFrame', col_name: str) -> int:
+        '''
+        Calculate median values per specified numeric column.
+
+            Parameters:
+            -----------
+            df : DataFrame
+                The input DataFrame to preprocess.
+            col_name : str
+                The name of the column to be processed.
+
+            Returns:
+            -----------
+            col_median_int : int
+                The calculated median value for the specified column.
+        '''
+        # Compute median for the target column.
+        col_median_int = int(df.approxQuantile(col_name, [0.5], 0.0)[0])
+        DataUtils.logger.info(f'Median: {col_name} = {col_median_int}')
+        return col_median_int
+
+
+
+
+
+
+
+
+
+    @staticmethod
+    def engineer_features(df: 'DataFrame', **kwargs) -> 'DataFrame':
+        '''
+        Performs feature engineering on the dataset. Scale numerical columns,
+        encode categorical features, and merge metadata from directing/writing tables.
+
+        PROCEDURE:
+        (1) Merge directing and writing metadata.
+        (2) Convert years to categorical features.
+        (3) Scale numeric features.
+        (4) Set up Spark pipeline.
+        (5) Fit and transform the data.
+        (6) Drop redundant columns.
+
+            Parameters:
+            -----------
+            df : DataFrame
+                The input Spark DataFrame (ideally - processed data).
+            kwargs : dict
+                Dictionary containing dataframes for JSON metadata.
+
+            Returns:
+            -----------
+            df : DataFrame
+                The transformed DataFrame with engineered features.
+        '''
+        # (1) Merge directing and writing metadata.
+        writing_df = kwargs.get('writing_df')
+        writing_df = writing_df.withColumnRenamed('movie', 'tconst').withColumnRenamed('writer', 'writer_id')
+        df = df.join(writing_df, on='tconst', how='left')
+
+
+
+        # directing_df = kwargs.get('directing_df')
+        # directing_df.printSchema()
+        # writing_df.printSchema()
+        # df = df.join(directing_df, on='tconst', how='left')
+        # "Unknown" for missing director/writer IDs.
+        # df = df.fillna({'director_id': 'Unknown', 'writer_id': 'Unknown'})
+        # (2) Convert "startYear" and "endYear" into categorical features to avoid misleading numerical relationships.
+        start_year_indexer = StringIndexer(inputCol='startYear', outputCol='startYear_idx', handleInvalid='keep')
+        end_year_indexer = StringIndexer(inputCol='endYear', outputCol='endYear_idx', handleInvalid='keep')
+        start_year_encoder = OneHotEncoder(inputCol='startYear_idx', outputCol='startYear_encoded')
+        end_year_encoder = OneHotEncoder(inputCol='endYear_idx', outputCol='endYear_encoded')
+        # (3) Convert "runtimeMinutes" and "numVotes" into a single feature vector and apply standard scaling
+        # to prevent larger values (numVotes) from dominating smaller ones (runtimeMinutes), ensuring
+        # balanced feature contributions in ML models.
+        assembler = VectorAssembler(inputCols=['runtimeMinutes', 'numVotes'], outputCol='raw_features')
+        scaler = StandardScaler(inputCol='raw_features', outputCol='scaled_features')
+        # (4) Set up Spark pipeline.
+        pipeline = SparkPipeline(stages=[
+            start_year_indexer,
+            end_year_indexer,
+            start_year_encoder,
+            end_year_encoder,
+            assembler,
+            scaler
+        ])
+        # (5) Fit and transform the data.
+        df = pipeline.fit(df).transform(df)
+        # (6) Drop redundant columns, disregarded after feature engineering.
+        df = df.drop("startYear", "endYear", "startYear_idx", "endYear_idx", "raw_features")
         return df
--- movie-pipeline/pipeline.py
+++ movie-pipeline/pipeline.py
@@ -20,7 +20,9 @@ import argparse
 from data_utils import DataUtils
 from datetime import datetime
 from logger import get_logger
+import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
 
 
 class Pipeline:
@@ -29,7 +31,7 @@ class Pipeline:
     Pipeline - Main class orchestrating the overall workflow, utilizing DataUtils
                and Classifier classes:
                (1) Initial setups: CLI args, access data paths, etc.
-               (2) Loading CSV/JSON data.
+               (2) Loading data (from CSV/JSON).
                (3) Process and train the model with Classifier.
                (3) Evaluates and save predictions.
     -------------------------
@@ -39,11 +41,18 @@ class Pipeline:
     logger = get_logger(__name__)
     # Base path for results to be customized for the task-specific data.
     RESULT_BASE_PATH = r'results/{data_path}/{set_name}_prediction_{timestamp}.csv'
+    # Standard numeric cols in data.
+    NUMERIC_COLS = ['runtimeMinutes', 'numVotes', 'startYear', 'endYear']
 
     def __init__(self: 'Pipeline') -> None:
         '''
         Initialize the Pipeline class.
         '''
+        self.df = None
+        self.median_dict = {
+            'runtimeMinutes': None,
+            'numVotes': None
+        }
         # Set up CLI args.
         self.args = Pipeline.set_args()
         # Extract and assign data paths for the task.
@@ -127,13 +136,72 @@ class Pipeline:
         pred_path = pred_path.replace('{timestamp}', curr_time)
         return pred_path
 
+    def preprocess(self: 'Pipeline', df: 'DataFrame', train: bool=False) -> 'DataFrame':
+        '''
+        Preprocess the data by handling missing values, fixing data types,
+        normalizing text fields, handling outliers, etc. Utilizes various
+        atomic functions from DataUtils.
+
+            FULL PROCEDURE:
+            (1) Pre-process numeric columns by converting them to integer type.
+            (2) Inject median values for missing records: runtimeMinutes and numVotes.
+            (3) Ensure startYear ≤ endYear and handle missing values.
+            (4) Normalize text fields ("primaryTitle", "originalTitle").
+
+            Parameters:
+            -----------
+            df : DataFrame
+                The input dataframe to preprocess.
+            train : bool (default=False)
+                Flag to indicate if the input data is the TRAIN set.
+
+            Returns:
+            -----------
+            (DataFrame, medians) : tuple
+                Tuple with the preprocessed DataFrame and optionally medians
+        '''
+        # (1) Pre-process numeric columns.
+        df = DataUtils.preprocess_numeric_cols(df, self.NUMERIC_COLS)
+        # (2) Inject median values for missing records: "runtimeMinutes" and "numVotes".
+        for col_name in ['runtimeMinutes', 'numVotes']:
+            # From TRAIN: find medians for numeric columns (for further injection).
+            # For other sets, assign pre-computed values, since TRAIN runs first.
+            if train:
+                self.median_dict[col_name] = DataUtils.calc_median_col(df, col_name)
+            # Inject TRAIN median values into NULL fields.
+            df = df.withColumn(
+                col_name, when(col(col_name).isNull(), self.median_dict[col_name]).otherwise(col(col_name))
+            )
+        # (3) Ensure startYear ≤ endYear and handle missing values.
+        df = df.withColumn('startYear', when(col('startYear').isNull(), col('endYear')).otherwise(col('startYear')))
+        df = df.withColumn('endYear', when(col('endYear').isNull(), col('startYear')).otherwise(col('endYear')))
+        df = df.withColumn('endYear', when(col('endYear') < col('startYear'), col('startYear')).otherwise(col('endYear')))
+        # (4) Normalize text title fields and handle missing records.
+        df = df.toPandas()
+        df['primaryTitle'] = df['primaryTitle'].apply(DataUtils.preprocess_text)
+        df['originalTitle'] = df['originalTitle'].apply(DataUtils.preprocess_text)
+        df['primaryTitle'] = df.apply(
+            lambda row: row['originalTitle'] if pd.isna(row['primaryTitle']) else row['primaryTitle'], axis=1
+        )
+        df['originalTitle'] = df.apply(
+            lambda row: row['primaryTitle'] if pd.isna(row['originalTitle']) else row['originalTitle'], axis=1
+        )
+        # (Back to Spark DataFrame.)
+        df = SparkSession.builder.getOrCreate().createDataFrame(df)
+        return df
+
     def __call__(self: 'Pipeline') -> None:
         '''
-        Main method to call the pipeline functionalities:
+        Main method to call the pipeline functionalities.
+
+        PROCEDURE:
         (1) Load data.
+        (2) Pre-process data.
+        (3) Apply feature engineering.
+        (4) TODO:
         '''
+        # (1) Load data.
         Pipeline.logger.info('LOADING DATA...')
-        # Load data.
         data = DataUtils.load_data(
             spark=self.spark,
             train_path=self.train_csv_path,
@@ -143,19 +211,26 @@ class Pipeline:
             writing_path=self.writing_json_path
         )
         Pipeline.logger.info('DATA LOADED!')
-        # Pre-process data.
-        Pipeline.logger.info('PREPROCESSING DATA...')
-        # data['train'].show(20, truncate=False)
-        train_df = DataUtils.preprocess(data['train'])
+        # (2) Pre-process data: TRAIN, VAL, TEST.
+        Pipeline.logger.info('PRE-PROCESSING DATA...')
+        train_df = self.preprocess(data['train'], train=True)
+        val_df = self.preprocess(data['val'])
+        test_df = self.preprocess(data['test'])
+        Pipeline.logger.info('DATA PRE-PROCESSING COMPLETE!')
+        # (3) TODO: Apply feature engineering procedures.
+
+        # Pipeline.logger.info('APPLYING FEATURE ENGINEERING...')
+        # train_df = DataUtils.engineer_features(
+        #     df=train_df, directing_df=data['directing'], writing_df=data['writing']
+        # )
         # train_df.show(20, truncate=False)
-        val_df = DataUtils.preprocess(data['val'])
-        test_df = DataUtils.preprocess(data['test'])
-        # Apply feature engineering procedures.
-        Pipeline.logger.info('APPLYING FEATURE ENGINEERING...')
-        # train_df = DataUtils.feature_engineering(train_df)
-        # val_df = DataUtils.feature_engineering(val_df)
-        # test_df = DataUtils.feature_engineering(test_df)
-        Pipeline.logger.info('DATA PREPARATION COMPLETE!')
+        # val_df = DataUtils.engineer_features(
+        #     df=val_df, directing_df=data['directing'], writing_df=data['writing']
+        # )
+        # test_df = DataUtils.engineer_features(
+        #     df=test_df, directing_df=data['directing'], writing_df=data['writing']
+        # )
+        Pipeline.logger.info('FEATURE ENGINEERING COMPLETE!')
 
 
 if __name__ == '__main__':
