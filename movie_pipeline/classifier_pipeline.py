272cd86cc801102aed5d106cf9316f2e38af9143
chizo <cierkoszfilip@gmail.com>
2025-03-17 12:38:28 +0100

    WIP: Fix feature engineering in pipeline.

--- movie_pipeline/classifier_pipeline.py
+++ movie_pipeline/classifier_pipeline.py
@@ -22,7 +22,7 @@ from datetime import datetime
 from logger import get_logger
 from pyspark.ml import Pipeline as SparkPipeline
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.ml.feature import StringIndexer, VectorAssembler, StandardScaler
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, when, collect_list, concat_ws
 
@@ -34,7 +34,7 @@ class ClassifierPipeline:
                          DataUtils and Classifier classes:
                          (1) Initial setups: CLI args, access data paths, etc.
                          (2) Loading data (from CSV/JSON).
-                         (3) Process and train the model with Classifier.
+                         (3) Train the Random Forest Classifier model.
                          (3) Evaluates and save predictions.
     -------------------------
     '''
@@ -46,8 +46,8 @@ class ClassifierPipeline:
     # Base path for trained model.
     MODEL_BASE_PATH = r'models/{data_path}/'
     # Standard cols to follow in data.
-    NUMERIC_COLS = ['runtimeMinutes', 'numVotes', 'startYear', 'endYear']
-    FEATURE_COLS = ['runtimeMinutes', 'numVotes', 'startYear', 'endYear', 'writers_index', 'directors_index']
+    NUMERIC_COLS = {'runtimeMinutes', 'numVotes', 'startYear', 'endYear'}
+    CATEGORICAL_COLS = {'primaryTitle', 'originalTitle', 'writers', 'directors'}
 
     def __init__(self: 'ClassifierPipeline') -> None:
         '''
@@ -62,12 +62,13 @@ class ClassifierPipeline:
         self.args = ClassifierPipeline.set_args()
         # Extract and assign data paths for the task.
         self.data_path = self.args.data
-        self.model_path = ClassifierPipeline.MODEL_BASE_PATH.replace('{data_path}', self.data_path)
         self.train_csv_path = f'{self.data_path}/train-*.csv'
         self.val_csv_path = f'{self.data_path}/{self.args.val}'
         self.test_csv_path = f'{self.data_path}/{self.args.test}'
         self.directing_json_path = f'{self.data_path}/{self.args.directing}'
         self.writing_json_path = f'{self.data_path}/{self.args.writing}'
+        # Set up model store path.
+        self.model_path = ClassifierPipeline.MODEL_BASE_PATH.replace('{data_path}', self.data_path)
         # Initialize VAL and TEST results files.
         self.val_pred_path = self.set_pred_file(set_name='val', data_path=self.data_path)
         self.test_pred_path = self.set_pred_file(set_name='test', data_path=  self.data_path)
@@ -75,12 +76,14 @@ class ClassifierPipeline:
         self.spark = SparkSession.builder.appName('MoviePipeline').getOrCreate()
         # Initialize RF classifier.
         self.rf_classifier = RandomForestClassifier(
-            featuresCol='features',
+            featuresCol='scaled_features',
             labelCol='label',
             numTrees=100,  # TODO: Number of trees in the forest.
-            maxDepth=10,   # TODO: Depth of each tree (to prevent overfitting).
+            # maxDepth=10,   # TODO: Depth of each tree (to prevent overfitting).
             seed=42        # TODO: Random seed for reproducibility.
         )
+        # Initialize feature cols that will be further extended.
+        self.feature_cols = self.NUMERIC_COLS
 
     @staticmethod
     def set_args() -> argparse.Namespace:
@@ -157,7 +160,7 @@ class ClassifierPipeline:
         '''
         # Load JSON files with metadata for directing and writing.
         writing_df = DataUtils.load_json(self.spark, self.writing_json_path)
-        # For directing JSON, merge movie and director dictionaries.
+        # For directing: merge movie and director dictionaries (since unstructured).
         directing_df = DataUtils.merge_directing_json(self.spark, self.directing_json_path)
         # Load CSV files.
         val_df = DataUtils.load_csv(self.spark, self.val_csv_path)
@@ -216,27 +219,23 @@ class ClassifierPipeline:
         df = DataUtils.normalize_text_cols(df)
         return df
 
-    def engineer_features(self: 'ClassifierPipeline', df: 'DataFrame') -> 'DataFrame':
+    def merge_metadata_into_df(self: 'ClassifierPipeline', df: 'DataFrame') -> 'DataFrame':
         '''
-        Apply feature engineering to the DataFrame, handling metadata.
-
-            PROCEDURE:
-            (1) Merging metadata (directors, writers) to include relevant categorical features.
-            (2) Handling categorical features via indexing.
-            (3) Assembling numerical features into a vector for RandomForestClassifier.
+        Merge movie metadata - wrting and directing - into main DataFrame.
 
             Parameters:
             -----------
             df : DataFrame
-                The input dataframe to be transformed.
+                The main DataFrame to merge the metadata into.
 
             Returns:
             -----------
             df : DataFrame
-                The transformed DataFrame ready for training.
+                DataFrame including directing and writing metadata.
         '''
-        # (1) Join movie dataset with writing and directing metadata. Some movies have
-        #     multiple writers and/or directors, thus we need to first aggregate them.
+        ClassifierPipeline.logger.info('MERGING METADATA INTO MAIN DATAFRAME...')
+        # Join movie dataset with writing and directing metadata. Some movies have
+        # multiple writers and/or directors, thus we need to first aggregate them.
         writing_df = self.data_dict['writing'].groupBy('movie') \
             .agg(collect_list('writer').alias('writers_array')) \
             .withColumn('writers', concat_ws(',', 'writers_array')) \
@@ -250,16 +249,74 @@ class ClassifierPipeline:
         # Handle NULL cols for writers and directors.
         df = df.withColumn('writers', when(col('writers').isNull(), 'unknown').otherwise(col('writers')))
         df = df.withColumn('directors', when(col('directors').isNull(), 'unknown').otherwise(col('directors')))
+        ClassifierPipeline.logger.info('MERGING METADATA: COMPLETE!')
+        return df
+
+    def engineer_features(self: 'ClassifierPipeline', df: 'DataFrame', train:bool=False) -> 'DataFrame':
+        '''
+        Apply feature engineering to the DataFrame, handling metadata.
+
+            PROCEDURE:
+            (1) Merging metadata (directors, writers) to include relevant categorical features.
+            (2) For TRAIN: convert booleam string labels to binary numeric.
+
+            (2) Handling categorical features via indexing.
+            (3) Assembling numerical features into a vector for RandomForestClassifier.
+
+            Parameters:
+            -----------
+            df : DataFrame
+                The input dataframe to be transformed.
+            train : bool (default=False)
+                Flag to indicate if the input data is the TRAIN set.
+
+            Returns:
+            -----------
+            df : DataFrame
+                The transformed DataFrame ready for training.
+        '''
+        # (1) Join movie dataset with writing and directing metadata.
+        df = self.merge_metadata_into_df(df)
+        # (2) For TRAIN: convert string labels to binary numeric, where: "True" -> 1, "False" -> 0.
+        if train:
+            df = df.withColumn('label', col('label').cast('double'))
+        df.show(10, False)
         # (2) Handle categorical variables by indexing them.
-        indexer_writers = StringIndexer(inputCol='writers', outputCol='writers_index', handleInvalid='keep')
-        indexer_directors = StringIndexer(inputCol='directors', outputCol='directors_index', handleInvalid='keep')
-        df = indexer_writers.fit(df).transform(df)
-        df = indexer_directors.fit(df).transform(df)
+        for col_name in self.CATEGORICAL_COLS:
+            output_col_name = f'{col_name}_index'
+            indexer = StringIndexer(inputCol=col_name, outputCol=output_col_name).setHandleInvalid('keep')
+            df = indexer.fit(df).transform(df)
+            df = df.drop(col_name)
+            # Append the new output column into set of features.
+            self.feature_cols.add(output_col_name)
+        print(self.feature_cols)
+        df.show(10, False)
+
+        ###### test from here
+
+        # # TODO: (3)
+        # feature_cols = [col_name for col_name in df.columns if col_name != "label"]
+        # assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
+        # df = assembler.transform(df)
+        # # Standardize numeric features
+        # scaler = StandardScaler(inputCol="features", outputCol="scaled_features", withStd=True, withMean=False)
+        # scaler_model = scaler.fit(df)
+        # df = scaler_model.transform(df)
+        ###### to here
+        # indexer_writers = StringIndexer(inputCol='writers', outputCol='writers_index', handleInvalid='keep')
+        # indexer_directors = StringIndexer(inputCol='directors', outputCol='directors_index', handleInvalid='keep')
+        # df = indexer_writers.fit(df).transform(df)
+        # df = indexer_directors.fit(df).transform(df)
         # (3) Assemble all feature columns into a single feature vector.
-        assembler = VectorAssembler(inputCols=self.FEATURE_COLS, outputCol='features')
-        df = assembler.transform(df)
+        # assembler = VectorAssembler(inputCols=self.FEATURE_COLS, outputCol='features')
+        # df = assembler.transform(df)
+        # (4) For TRAIN: convert string labels to binary numeric, where: "True" -> 1, "False" -> 0.
+        # if train:
+        #     df = df.withColumn('label', when(col('label') == 'True', 1).otherwise(0))
+        #     df = df.withColumn('label', col('label').cast(IntegerType()))
         # Drop unused columns (they were converted to indexed features).
-        df = df.drop('writers', 'directors')
+        # df = df.drop('writers', 'directors')
+        # df.show(20, False)
         return df
 
     def train_model(self: 'ClassifierPipeline', df: 'DataFrame') -> 'PipelineModel':
@@ -293,7 +350,7 @@ class ClassifierPipeline:
             (1) Load data.
             (2) Pre-process data.
             (3) Apply feature engineering.
-            (4) TODO: Train model.
+            (4) Train RF classifier model.
             (5) TODO: Predict via model.
         '''
         # (1) Load data.
@@ -308,14 +365,14 @@ class ClassifierPipeline:
         ClassifierPipeline.logger.info('DATA PRE-PROCESSING: COMPLETE!')
         # (3) Apply feature engineering procedures.
         ClassifierPipeline.logger.info('APPLYING FEATURE ENGINEERING...')
-        train_df = self.engineer_features(train_df)
-        val_df = self.engineer_features(val_df)
-        test_df = self.engineer_features(test_df)
+        train_df = self.engineer_features(train_df, train=True)
+        # val_df = self.engineer_features(val_df)
+        # test_df = self.engineer_features(test_df)
         ClassifierPipeline.logger.info('FEATURE ENGINEERING: COMPLETE!')
         # (4) TODO: Train the model.
-        # ClassifierPipeline.logger.info('TRAINING RANDOM FORREST MODEL...')
+        ClassifierPipeline.logger.info('TRAINING RANDOM FORREST MODEL...')
         # rf_model = self.train_model(train_df)
-        # ClassifierPipeline.logger.info('MODEL TRAINING: COMPLETE!')
+        ClassifierPipeline.logger.info('MODEL TRAINING: COMPLETE!')
 
 
 if __name__ == '__main__':
